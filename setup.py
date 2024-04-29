ed08b489098944d6dbdef5afce042976f5e5012b
rlan <brianlanbo@gmail.com>
2024-04-29 15:50:49 +0800

    version to 0.1.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rlanutils',
-    version='0.1.7',
+    version='0.1.8',
     packages=find_packages(),
     description='A handy tool that make your day to day programming much easier. ',
     long_description=open('README.md').read(),
