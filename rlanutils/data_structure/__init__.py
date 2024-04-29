007aa7d0933529b4d2e72819902e6b1d6d831a06
rlan <brianlanbo@gmail.com>
2024-04-29 11:31:52 +0800

    removed __all__ in __init__.py

--- rlanutils/data_structure/__init__.py
+++
@@ -1,3 +0,0 @@
-from .set import find_consecutive_subsets
-
-__all__ = ["find_consecutive_subsets"]
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rlanutils',
-    version='0.1.6',
+    version='0.1.7',
     packages=find_packages(),
     description='A handy tool that make your day to day programming much easier. ',
     long_description=open('README.md').read(),
