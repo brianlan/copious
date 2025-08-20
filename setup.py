61d496a0c1976548b55e3e0899c2d72584e12b14
rlan <brianlanbo@gmail.com>
2025-08-20 11:44:18 +0800

    added write_pcd and read_pcd and updated the requirements a bit.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='copious',
-    version='0.1.28',
+    version='0.1.29',
     packages=find_packages(),
     description='A handy tool that make your day to day programming much easier. ',
     long_description=open('README.md').read(),
@@ -12,12 +12,18 @@ setup(
     author_email='brianlanbo@gmail.com',
     url='https://github.com/brianlan/copious',
     install_requires=[
-        "numpy",
-        "scipy",
-        "tqdm",
-        "matplotlib",
-        "pytest",
-        "pyyaml",
-        "opencv-python",
+        "numpy>=1.21.0",
+        "scipy>=1.7.0", 
+        "tqdm>=4.60.0",
+        "matplotlib>=3.5.0",
+        "pyyaml>=5.4.0",
+        "opencv-python>=4.5.0",
+        "polars>=0.19.0",
+        "pypcd4>=1.0.0",
     ],
+    extras_require={
+        "dev": [
+            "pytest>=7.0.0",
+        ],
+    },
 )
