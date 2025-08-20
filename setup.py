5656913a3f98fa906342c6ed241122c142e111c7
rlan <brianlanbo@gmail.com>
2025-08-20 12:23:52 +0800

    added min version test.

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ setup(
     author_email='brianlanbo@gmail.com',
     url='https://github.com/brianlan/copious',
     install_requires=[
-        "numpy>=1.21.0",
-        "scipy>=1.7.0", 
-        "tqdm>=4.60.0",
-        "matplotlib>=3.5.0",
-        "pyyaml>=5.4.0",
-        "opencv-python>=4.5.0",
-        "polars>=0.19.0",
-        "pypcd4>=1.0.0",
+        "numpy>=1.22",
+        "scipy>=1.8",
+        "tqdm>=4.62",
+        "matplotlib>=3.5",
+        "pyyaml>=6.0",
+        "opencv-python>=4.6",
+        "polars>=0.20",
+        "pypcd4>=1.0",
     ],
     extras_require={
         "dev": [
